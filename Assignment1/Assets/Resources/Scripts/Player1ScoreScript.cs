38d9fbacf319a265c3e6fe522bd5aa6cb629dd2e
abivella <abigail.vella.b42203@mcast.edu.mt>
2017-12-16 11:20:23 +0100

    -- Added background for the end scene -- Added game over text, play again text and quit text -- If play again is clicked, the start scene will load -- If quit is clicked on, the user will exit from the game -- Changed colours and fonts of the text in the end scene

---
+++ Assignment1/Assets/Resources/Scripts/Player1ScoreScript.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player1ScoreScript : MonoBehaviour {
+
+    BallScript ballScript = new BallScript();
+    public Player1PaddleScript paddlePlayer1;
+    public GameObject ball;
+    static int player1Score;
+    int goal;
+    public int speed;
+    private Vector3 initialPosition;
+
+    LevelManager levelManager = new LevelManager();
+
+    // Use this for initialization
+    void Start () {
+
+        Vector2 zero = new Vector2(0, 0);
+        initialPosition = zero;
+        //print(zero);
+        ball = GameObject.FindGameObjectWithTag("ball");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if(goal == 3)
+        {
+            levelManager.LoadNextScene();
+        }
+	}
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        print("Collision");
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        goal++;
+        if(levelManager.GetScene() == "Level1")
+        {
+            player1Score++;
+        }
+        else if(levelManager.GetScene() == "Level2")
+        {
+            player1Score += 2;
+        }
+        else if(levelManager.GetScene() == "Level3")
+        {
+            player1Score += 3;
+        }
+       
+        print("Player 1 Score: " + player1Score);
+
+        ball.transform.position = initialPosition;
+        ball.GetComponent<Rigidbody2D>().velocity = new Vector2(speed, -1f);
+
+
+    }
+}
---
+++ Assignment1/Assets/Resources/Scripts/Player2ScoreScript.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player2ScoreScript : MonoBehaviour {
+
+    BallScript BallScript = new BallScript();
+    static int player2Score;
+    public GameObject ball;
+    private Vector3 initialPosition;
+    public int speed;
+    int goal;
+
+    LevelManager levelManager = new LevelManager();
+
+
+    // Use this for initialization
+    void Start () {
+
+        Vector2 zero = new Vector2(0, 0);
+        initialPosition = zero;
+        //print(zero);
+        ball = GameObject.FindGameObjectWithTag("ball");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (player2Score == 5)
+        {
+            levelManager.LoadNextScene();
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        print("Collision");
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        goal++;
+        if (levelManager.GetScene() == "Level1")
+        {
+            player2Score++;
+        }
+        else if (levelManager.GetScene() == "Level2")
+        {
+            player2Score += 2;
+        }
+        else if (levelManager.GetScene() == "Level3")
+        {
+            player2Score += 3;
+        }
+
+        player2Score++;
+        print("Player 2 Score: " + player2Score);
+
+        ball.transform.position = initialPosition;
+        ball.GetComponent<Rigidbody2D>().velocity = new Vector2(speed, -1f);
+
+    }
+}
