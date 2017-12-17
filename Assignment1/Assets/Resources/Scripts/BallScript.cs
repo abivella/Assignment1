2f094408597f9d9eac1c3f7d0415e88325b90a97
abivella <abigail.vella.b42203@mcast.edu.mt>
2017-12-17 20:39:09 +0100

    -- Fixed obstacles in Level3. Obstacles will move up and down continously on start of Level3

--- Assignment1/Assets/Resources/Scripts/BallScript.cs
+++ Assignment1/Assets/Resources/Scripts/BallScript.cs
@@ -11,14 +11,11 @@ public class BallScript : MonoBehaviour {
     public int player1Score = 0;
     public int player2Score = 0;
     public float speed;
-    public GameObject obstacle;
     LevelManager levelManager = new LevelManager();
 
     // Use this for initialization
     void Start () {
 
-        obstacle = GameObject.FindGameObjectWithTag("obstacle");
-        moveObstacles();
         paddlePlayer1 = GameObject.FindObjectOfType<Player1PaddleScript>();
 
         paddleBallPosDiff = this.transform.position - paddlePlayer1.transform.position;
@@ -59,38 +56,11 @@ public class BallScript : MonoBehaviour {
     {
         string colName = collision.collider.name;
 
-        if (gameStart && (colName == "PaddlePlayer1" || colName == "PaddlePlayer2" || colName == "TopBorder" || colName == "BottomBorder" || colName == "RightBorder1" || colName == "RightBorder2" || colName == "LeftBorder1" || colName == "LeftBorder2" || colName == "Obstacle1" || colName == "Obstacle2"))
+        if (gameStart && (colName == "PaddlePlayer1" || colName == "PaddlePlayer2" || colName == "TopBorder" || colName == "BottomBorder" || colName == "RightBorder1" || colName == "RightBorder2" || colName == "LeftBorder1" || colName == "LeftBorder2" || colName == "Obstacle1" || colName == "Obstacle (1)"))
         {
             //GetComponent<AudioSource>().Play();
             Vector2 tweak = new Vector2(Random.Range(-0.2f, 0.2f), Random.Range(0f, 0.2f));
             this.GetComponent<Rigidbody2D>().velocity += tweak;
         }
     }
-
-    public void moveObstacles()
-    {
-        if (levelManager.GetScene() == "Level3")
-        {
-            obstacle.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -1f);
-        }
-    }
-
-
-
-
-
-    /*if (!gameStart) //if(gameStart==false)
-        {
-            this.transform.position = paddlePlayer1.transform.position + paddleBallPosDiff;
-        }
-
-        if (!gameStart && Input.GetMouseButtonDown(0))
-        {
-            gameStart = true;
-            this.GetComponent<Rigidbody2D>().velocity = new Vector2(5f, -1f);
-        }*/
-
-    //paddlePlayer1 = GameObject.FindObjectOfType<Player1PaddleScript>();
-
-    //paddleBallPosDiff = this.transform.position - paddlePlayer1.transform.position;
 }
---
+++ Assignment1/Assets/Resources/Scripts/ObstacleScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleScript : MonoBehaviour {
+
+    //private Vector2 startPosition;
+    float speed = 4f;
+
+    public Transform obstacle; 
+    public Transform destinationTrans;
+
+    Vector3 start;
+    Vector3 destination;
+    Vector3 nextDestination;
+
+	// Use this for initialization
+	void Start () {
+        start = obstacle.position;
+        destination = destinationTrans.position;
+
+        nextDestination = destination;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        obstacle.position = Vector3.MoveTowards(obstacle.position, nextDestination, speed * Time.deltaTime);
+        if (Vector3.Distance(obstacle.position, nextDestination) <=0.1)
+        {
+            if(nextDestination == destination)
+            {
+                nextDestination = start;
+            }
+            else if(nextDestination == start)
+            {
+                nextDestination = destination;
+            }
+        }
+	}
+}
