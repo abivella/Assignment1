a84a6f5ed1f2a41b4aa26e83eafac4baaddb141e
abivella <abigail.vella.b42203@mcast.edu.mt>
2017-12-17 10:57:51 +0100

    -- Placed text in each level indicating the current level -- Placed text to show the score of each player in each level -- Placed text to show the final score of each player in the end scene

---
+++ Assignment1/Assets/Resources/Scripts/PlayerScoresTextScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerScoresTextScript : MonoBehaviour {
+
+    public Text player1Score;
+    public Text player2Score;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        player1Score.text = "Player 1: " + Player1ScoreScript.player1Score;
+        player2Score.text = "Player 2: " + Player2ScoreScript.player2Score;
+	}
+}
--- Assignment1/Assets/Resources/Scripts/WinTextScript.cs
+++ Assignment1/Assets/Resources/Scripts/WinTextScript.cs
@@ -6,6 +6,8 @@ using UnityEngine.UI;
 public class WinTextScript : MonoBehaviour {
 
     public Text winner;
+    public Text player1Score;
+    public Text player2Score;
 
     // Use this for initialization
     void Start () {
@@ -14,6 +16,10 @@ public class WinTextScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
+        player1Score.text = "Player 1: " + Player1ScoreScript.player1Score;
+        player2Score.text = "Player 2: " + Player2ScoreScript.player2Score;
+
         if (Player1ScoreScript.player1Score > Player2ScoreScript.player2Score)
         {
             winner.text = "PLAYER 1 IS THE WINNER";
